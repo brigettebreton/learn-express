587b4f708a9a3e7f0ce96c2b5b921f4797712d40
Gigi Breton <gigibreton@MacBook-Air-2.local>
2024-03-12 18:09:53 -0400

    In class activities

--- router-ex-server.js
+++ router-ex-server.js
@@ -3,6 +3,8 @@ const app = express();
 const port = 8001;
 const birds = require('./birds');
 
+//looks for url to end in /birds
+//ex: localhost:8001 "fails to get", while localhost:8001/birds shows the birds.js home page
 app.use('/birds', birds);
 
 app.listen(port, () => {
